3c6003d0f2decd8ecedafa7ab28ca3359205e9a3
Akshay2210 <62205256+Akshay2210@users.noreply.github.com>
2021-04-21 18:24:32 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -19,12 +19,10 @@ image = Image.open(imagename)
 
 exifdata = image.getexif()
 
-# iterating over all EXIF data fields
 for tag_id in exifdata:
-    # get the tag name, instead of human unreadable tag id
     tag = TAGS.get(tag_id, tag_id)
     data = exifdata.get(tag_id)
-    # decode bytes 
+ 
     if isinstance(data, bytes):
         data = data.decode()
     print(f"{tag:25}: {data}")
